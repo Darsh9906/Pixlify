b021e8ee5259ed9ffc6846fc642d65e893a504e1
Darsh <darshbhatt089@gmail.com>
2026-05-08 02:14:24 +0530

    Api-key tested and Added hover effects

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 lucide.createIcons();
+
+const imageGrid = document.getElementById('imageGrid')
+
+const Access_Key = "KEv6T0R2L5Mkzsd5X-2fa9i4q4qqKkBqFaREaHwXyJ0";
+
+async function getImages() {
+
+    const response = await fetch("https://api.unsplash.com/photos/random?count=12",
+        {
+            headers: {
+                Authorization: `Client-ID ${Access_Key}`
+            }
+        }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+    
+}
+
+getImages();
