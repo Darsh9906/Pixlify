cf96735a06989337a12a170307e43bb724fd8682
Darsh <darshbhatt089@gmail.com>
2026-05-12 23:56:24 +0530

    Added loop to render images

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 lucide.createIcons();
 
 const imageGrid = document.getElementById('imageGrid')
@@ -6,7 +7,7 @@ const Access_Key = "KEv6T0R2L5Mkzsd5X-2fa9i4q4qqKkBqFaREaHwXyJ0";
 
 async function getImages() {
 
-    const response = await fetch("https://api.unsplash.com/photos/random?count=12",
+    const response = await fetch("https://api.unsplash.com/photos/random?count=3",
         {
             headers: {
                 Authorization: `Client-ID ${Access_Key}`
@@ -18,6 +19,27 @@ async function getImages() {
 
     console.log(data);
     
+
+    data.forEach(photo => {
+        
+        const card = document.createElement("div");
+
+        card.classList.add("card");
+
+        card.innerHTML = `
+
+        <img src="${photo.urls.regular}">
+
+        <div class="card-info">
+
+        <h3>${photo.alt_description}</h3>
+        <p>By ${photo.user.name}</p>
+
+        </div>
+        `;
+
+        imageGrid.appendChild(card);
+    });
 }
 
 getImages();
